eecfaaf811b780737b51687e35581ff5cdda9524
Sushant <sushantshetty09@gmail.com>
2025-12-30 22:18:43 +0530

    . added

--- server/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerDonor, loginDonor } = require('../controllers/authController');
-
-router.post('/register', registerDonor);
-router.post('/login', loginDonor);
-
-module.exports = router;
